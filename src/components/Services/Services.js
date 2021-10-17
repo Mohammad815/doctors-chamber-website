500497a2005d26f2d285b6ecf931ae93fe42a15a
Mohammad815 <mohammadforhad176@gmail.com>
2021-10-18 03:02:59 +0600

    add mapping

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {useState,useEffect } from 'react'
 
 const Services = () => {
+    const [doctors, setdoctors]=useState([]);
+
+    useEffect(()=>{
+        fetch('./doctors.JSON')
+        .then(res=>res.json())
+        .then(data=>setdoctors(data))
+    },[])
+    console.log(doctors)
     return (
         <div>
             this is service section
